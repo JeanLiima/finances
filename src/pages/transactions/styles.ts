eff85a4a9cb0bef3840f33d761772e605a1dc2f4
JeanLiima <jean.car.liima@gmail.com>
2025-04-10 08:08:47 -0300

    Remove unnecessary styles

--- src/pages/transactions/styles.ts
+++ src/pages/transactions/styles.ts
@@ -10,23 +10,6 @@ const styles = StyleSheet.create({
 		marginTop: 8,
 		marginLeft: 8,
 		marginRight: 8,
-	},
-	addButton: {
-		alignSelf: "center",
-		marginBottom: 30,
-		marginTop: 15,
-		width: 60,
-		height: 60,
-		borderRadius: 30,
-		backgroundColor: "#3b3dbf",
-		justifyContent: "center",
-		alignItems: "center",
-		zIndex: 3, 
-		elevation: 5,
-		shadowColor: "#000",
-		shadowOffset: { width: 0, height: 2 },
-		shadowOpacity: 0.1,
-		shadowRadius: 4,
 	}
 });
 
