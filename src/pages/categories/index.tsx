4d8c2603b62dd0c24dfde6b4d42a4cbf8539a175
JeanLiima <jean.car.liima@gmail.com>
2025-04-10 08:09:13 -0300

    Create categories page

---
+++ src/pages/categories/index.tsx
@@ -0,0 +1,13 @@
+import { Text, View } from "react-native";
+
+import { styles } from "./styles";
+
+const Categories = () => {
+	return (
+		<View style={styles.container}>
+			<Text>User configurations</Text>
+		</View>
+	);
+};
+
+export { Categories };
